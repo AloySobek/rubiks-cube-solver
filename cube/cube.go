e57f21fc367c0e38a3972a3b9601efdebde9ec29
AloySobek <supernova.wandering.star@gmail.com>
2022-04-24 15:01:33 +0300

    feat(cube): All rotation operations with bitboard works correctly, improved kube output

--- cube/cube.go
+++ cube/cube.go
@@ -92,12 +92,52 @@ func Down(cube *Cube, reverse, double bool) {
 }
 
 func Print(cube *Cube) {
-	fmt.Printf("B: %064b\n\n", cube.Blue)
-	fmt.Printf("R: %064b\n\n", cube.Red)
-	fmt.Printf("O: %064b\n\n", cube.Orange)
-	fmt.Printf("G: %064b\n\n", cube.Green)
-	fmt.Printf("Y: %064b\n\n", cube.Yellow)
-	fmt.Printf("W: %064b\n\n", cube.White)
+	fmt.Println("Blue side:")
+	printSide(cube.Blue, "B")
+	fmt.Printf("\n\n")
+
+	fmt.Println("Red side:")
+	printSide(cube.Red, "R")
+	fmt.Printf("\n\n")
+
+	fmt.Println("Orange side:")
+	printSide(cube.Orange, "O")
+	fmt.Printf("\n\n")
+
+	fmt.Println("Green side:")
+	printSide(cube.Green, "G")
+	fmt.Printf("\n\n")
+
+	fmt.Println("Yellow side:")
+	printSide(cube.Yellow, "Y")
+	fmt.Printf("\n\n")
+
+	fmt.Println("White side:")
+	printSide(cube.White, "W")
+	fmt.Printf("\n\n")
+}
+
+func printSide(side uint64, name string) {
+	for i := 0; i < 8; i += 1 {
+		switch (side & (uint64(0xFF) << (8 * i))) >> (8 * i) {
+		case Blue:
+			fmt.Printf("B ")
+		case Red:
+			fmt.Printf("R ")
+		case Orange:
+			fmt.Printf("O ")
+		case Green:
+			fmt.Printf("G ")
+		case Yellow:
+			fmt.Printf("Y ")
+		case White:
+			fmt.Printf("W ")
+		}
+
+		if i == 3 {
+			fmt.Printf("%s ", name)
+		}
+	}
 }
 
 func rotateBlue(cube *Cube, reverse bool) {
--- main.go
+++ main.go
@@ -5,16 +5,41 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/urfave/cli/v2"
+
+	"github.com/AloySobek/Rubik/cube"
 )
 
 func solve(c *cli.Context) error {
+	if c.Args().Len() < 1 {
+		log.Fatal("Not enough arguments!")
+	}
+
 	mixSequence := c.Args().Get(0)
 
 	for _, move := range strings.Split(mixSequence, " ") {
 		fmt.Println("Move:", move)
 	}
+
+	cu := cube.Create()
+
+	cube.Print(cu)
+
+	fmt.Printf("\n\n\n")
+
+	start := time.Now()
+
+	for i := 0; i < 1000000000; i++ {
+		cube.Front(cu, true, true)
+	}
+
+	elapsed := time.Since(start)
+
+	cube.Print(cu)
+
+	fmt.Printf("Time to make move: %s\n", elapsed)
 
 	return nil
 }
