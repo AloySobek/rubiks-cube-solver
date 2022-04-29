30c3b450b67b7942e07c44f6fef4c32e0fda33c1
AloySobek <supernova.wandering.star@gmail.com>
2022-04-30 02:30:21 +0300

    feat(cube): Additional rotation functinos for convience

--- cube/notation.go
+++ cube/notation.go
@@ -47,3 +47,75 @@ func Down(cube *Cube, reverse, double bool) {
 		rotateWhite(cube, reverse)
 	}
 }
+
+func RFront(cube *Cube, _, _ bool) {
+	Front(cube, true, false)
+}
+
+func RRight(cube *Cube, _, _ bool) {
+	Right(cube, true, false)
+}
+
+func RLeft(cube *Cube, _, _ bool) {
+	Left(cube, true, false)
+}
+
+func RBack(cube *Cube, _, _ bool) {
+	Back(cube, true, false)
+}
+
+func RUp(cube *Cube, _, _ bool) {
+	Up(cube, true, false)
+}
+
+func RDown(cube *Cube, _, _ bool) {
+	Down(cube, true, false)
+}
+
+func DFront(cube *Cube, _, _ bool) {
+	Front(cube, false, true)
+}
+
+func DRight(cube *Cube, _, _ bool) {
+	Right(cube, false, true)
+}
+
+func DLeft(cube *Cube, _, _ bool) {
+	Left(cube, false, true)
+}
+
+func DBack(cube *Cube, _, _ bool) {
+	Back(cube, false, true)
+}
+
+func DUp(cube *Cube, _, _ bool) {
+	Up(cube, false, true)
+}
+
+func DDown(cube *Cube, _, _ bool) {
+	Down(cube, false, true)
+}
+
+func RDFront(cube *Cube, _, _ bool) {
+	Front(cube, true, true)
+}
+
+func RDRight(cube *Cube, _, _ bool) {
+	Right(cube, true, true)
+}
+
+func RDLeft(cube *Cube, _, _ bool) {
+	Left(cube, true, true)
+}
+
+func RDBack(cube *Cube, _, _ bool) {
+	Back(cube, true, true)
+}
+
+func RDUp(cube *Cube, _, _ bool) {
+	Up(cube, true, true)
+}
+
+func RDDown(cube *Cube, _, _ bool) {
+	Down(cube, true, true)
+}
